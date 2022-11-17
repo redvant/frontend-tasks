36389c5c00ddc14c1f46a28c1a11adbfc420ad33
Edgar Roman Cervantes <edgar.roman@jala.university>
2022-11-16 21:58:50 -0600

    Add fetch mocking config

---
+++ task12/react-router/src/mocks/handlers.js
@@ -0,0 +1,41 @@
+import { rest } from "msw";
+
+export const posts = [
+  {
+    id: 1,
+    userId: 1,
+    title: "Test post title",
+    body: "Test post body",
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "Test post title 2",
+    body: "Test post body 2",
+  },
+  {
+    id: 3,
+    userId: 2,
+    title: "Test post title 3",
+    body: "Test post body 3",
+  },
+];
+
+export const handlers = [
+  rest.get("https://jsonplaceholder.typicode.com/posts", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(posts));
+  }),
+  rest.post(
+    "https://jsonplaceholder.typicode.com/posts",
+    async (req, res, ctx) => {
+      const request = await req.json();
+      if (request.title === "Test error") {
+        return res(
+          ctx.status(403),
+          ctx.json({ errorMessage: "Error creating the post" })
+        );
+      }
+      return res(ctx.status(201), ctx.json({ ...req, id: 101 }));
+    }
+  ),
+];
