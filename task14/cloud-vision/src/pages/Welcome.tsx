42108a578be8ceb679c252d0d3d079ffaa6f5b71
Edgar Roman Cervantes <edgar.roman@jala.university>
2022-11-30 12:48:24 -0600

    Change html elements to materialUI components

--- task14/cloud-vision/src/pages/Welcome.tsx
+++ task14/cloud-vision/src/pages/Welcome.tsx
@@ -1,13 +1,23 @@
-import Button from '@mui/material/Button';
-import './Welcome.css'
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+// import "./Welcome.css";
 
 function Welcome() {
   return (
-    <div className='welcome-container'>
-      <h1>Detect the text in images with AI</h1>
-      <Button variant="contained">Start</Button>
-    </div>
-  )
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Typography variant="h2" align="center">
+        Detection of text in images with AI
+      </Typography>
+      <Button variant="contained" color="primary">Let's start</Button>
+    </Box>
+  );
 }
 
-export default Welcome
+export default Welcome;
