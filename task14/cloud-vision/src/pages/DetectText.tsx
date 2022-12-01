9ac741b4dc59a0af969b87f24d85412b7335968f
Edgar Roman Cervantes <edgar.roman@jala.university>
2022-11-30 20:07:36 -0600

    Add basic drop files area

--- task14/cloud-vision/src/pages/DetectText.tsx
+++ task14/cloud-vision/src/pages/DetectText.tsx
@@ -1,7 +1,49 @@
+import { Box, Typography, Button } from "@mui/material";
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["JPEG", "JPG", "PNG"];
 function DetectText() {
+  const [file, setFile] = useState<string | null>(null);
+
+  const handleChange = (file: File) => {
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      () => {
+        // convert image file to base64 string
+        let image64 = reader.result;
+        if (typeof image64 === "string") {
+          setFile(image64);
+        }
+      },
+      false
+    );
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div>DetectText</div>
-  )
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      minHeight="100vh"
+      gap="20px"
+    >
+      <Typography variant="h4">AI Text Detection</Typography>
+      <Box display="flex" gap="20px">
+        <FileUploader
+          multiple={false}
+          handleChange={handleChange}
+          name="file"
+          types={fileTypes}
+          classes="drop-area"
+        />
+      </Box>
+    </Box>
+  );
 }
 
-export default DetectText
+export default DetectText;
